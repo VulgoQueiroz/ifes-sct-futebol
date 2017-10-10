52a3359cadc2c0a75af7a5b94ebb0c3705578983
Diogo Souza <dqsouza10@gmail.com>
2017-10-10 10:11:37 -0300

    Commit Inicial

---
+++ jogador/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Jogador, Clube
+
+admin.site.register(Jogador)
+admin.site.register(Clube)# Register your models here.
---
+++ jogador/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Clube(models.Model):
+	nome = models.CharField(max_length=200)	
+
+class Jogador(models.Model):
+    nome = models.CharField(max_length=200)
+    idade = models.IntegerField()
+    altura = models.DecimalField(max_digits=3, decimal_places=2)
+
+    POSICOES = (
+    	('ATA', 'Atacante'),
+    	('MEI', 'Meio-Campo'),
+    	('ZAG', 'Zagueiro'),
+    	('LAT', 'Lateral'),
+    )
+
+    posicao = models.CharField(choices=POSICOES, max_length=3)
+    clube = models.ForeignKey(Clube)
+    ritmo = models.IntegerField() 
+    chute = models.IntegerField()
+    passe = models.IntegerField()
+    drible = models.IntegerField()
+    defesa = models.IntegerField() 
+    fisico = models.IntegerField()
+
+
+	
